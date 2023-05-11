65271ffab61d701bf5f6478ed37181be16f0e90b
Suryan S <suryannasa@gmail.com>
2023-05-12 01:53:02 +0530

    Modified setup

--- setup.py
+++ setup.py
@@ -1,15 +1,12 @@
-import os
+from setuptools import setup
 
-run_cmd_1 = 'python -m venv .venv'
-run_cmd_2 = 'source .venv/Scripts/activate'
-run_cmd_3 = 'pip install -r requirements.txt'
-run_cmd_4 = 'cd frontend'
-run_cmd_5 = 'npm install'
-run_cmd_6 = 'npm run build'
-
-jobs = [run_cmd_1, run_cmd_2, run_cmd_3, run_cmd_4, run_cmd_5, run_cmd_6]
-for i in range(len(jobs)): # type: ignore
-    if jobs[i].startswith(run_cmd_4):     
-        os.chdir('frontend')
-    else:
-        os.system(jobs[i])
+setup(
+    name='Precio',
+    version='0.0.2',
+    packages=['backend'],
+    url='',
+    license='MIT',
+    author='Suryan',
+    author_email='suryannasa@gmail.com',
+    description=''
+)
