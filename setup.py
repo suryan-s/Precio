d73cdb02aa262981435f7fd7114a7ef74867a71a
Suryan S <suryannasa@gmail.com>
2023-05-20 12:49:10 +0530

    UPDATED setup

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup
 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setup(
     name='Precio',
     version='0.0.2',
@@ -8,5 +11,9 @@ setup(
     license='MIT',
     author='Suryan',
     author_email='suryannasa@gmail.com',
-    description=''
+    description='''Precio is an open-source software tool for monitoring,
+    automating and visualizing farming data. It provides
+    real-time data analysis and insights of farming,
+    enabling informed decision-making and improving farming efficiency.''',
+    install_requires=requirements
 )
