7e5e6571057c844b7762ff29dfa96f86cb74453f
Arjun S <arjun8107@gmail.com>
2023-06-15 22:54:28 +0530

    Added Cards as components

--- frontend/src/components/dashboard/pages/Home.tsx
+++ frontend/src/components/dashboard/pages/Home.tsx
@@ -13,6 +13,42 @@ import {
 
 import Navbar from "@/components/dashboard/Navbar";
 
+interface ProjectCardProps {
+  name: string;
+  status: "online" | "offline";
+  image: string;
+}
+const ProjectCard = ({ name, status, image }: ProjectCardProps) => {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>{name}</CardTitle>
+        <CardDescription>
+          {status === "online" && (
+            <>
+              <span className="font-black text-xl leading-3 text-green-600">
+                •
+              </span>
+              Online
+            </>
+          )}
+          {status === "offline" && (
+            <>
+              <span className="font-black text-xl leading-3 text-gray-600">
+                •
+              </span>
+              Offline
+            </>
+          )}
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <img src={image} width={200} height={200} alt="Project Image" />
+      </CardContent>
+    </Card>
+  );
+};
+
 export default function Home() {
   const [token] = useLocalStorage<string | null>("token", null);
   const [__, setLocation] = useLocation();
@@ -25,44 +61,19 @@ export default function Home() {
   return (
     <>
       <Navbar />
-      <div className="flex flex-col justify-center p-6 px-6 md:px-12">
+      <div className="flex flex-col p-6 px-6 md:px-12 bg-slate-100 min-h-[calc(100vh-5rem)]">
         <h1 className="text-4xl font-bold">Projects</h1>
         <div className="mt-8 flex flex-wrap gap-8">
-          <Card>
-            <CardHeader>
-              <CardTitle>Project Name</CardTitle>
-              <CardDescription>
-                <span className="font-black text-xl leading-3">•</span> Offline
-              </CardDescription>
-            </CardHeader>
-            <CardContent>
-              <img
-                src="https://via.placeholder.com/200"
-                width={200}
-                height={200}
-                alt="Project Image"
-              />
-            </CardContent>
-          </Card>
-          <Card>
-            <CardHeader>
-              <CardTitle>Project Name</CardTitle>
-              <CardDescription>
-                <span className="font-black text-xl leading-3 text-green-600">
-                  •
-                </span>{" "}
-                Online
-              </CardDescription>
-            </CardHeader>
-            <CardContent>
-              <img
-                src="https://via.placeholder.com/200"
-                width={200}
-                height={200}
-                alt="Project Image"
-              />
-            </CardContent>
-          </Card>
+          <ProjectCard
+            name="Project Name"
+            status="offline"
+            image="https://via.placeholder.com/200"
+          />
+          <ProjectCard
+            name="Project Name"
+            status="online"
+            image="https://via.placeholder.com/200"
+          />
         </div>
       </div>
     </>
