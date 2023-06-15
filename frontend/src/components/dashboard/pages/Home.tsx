8239882cb096bbd65e685c6f433811927c1d7261
Arjun S <arjun8107@gmail.com>
2023-06-15 22:43:32 +0530

    Dashboard body added with cards

--- frontend/src/components/dashboard/pages/Home.tsx
+++ frontend/src/components/dashboard/pages/Home.tsx
@@ -2,6 +2,15 @@ import { useLocalStorage } from "usehooks-ts";
 import { useEffect } from "react";
 import { useLocation } from "wouter";
 
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
 import Navbar from "@/components/dashboard/Navbar";
 
 export default function Home() {
@@ -16,6 +25,46 @@ export default function Home() {
   return (
     <>
       <Navbar />
+      <div className="flex flex-col justify-center p-6 px-6 md:px-12">
+        <h1 className="text-4xl font-bold">Projects</h1>
+        <div className="mt-8 flex flex-wrap gap-8">
+          <Card>
+            <CardHeader>
+              <CardTitle>Project Name</CardTitle>
+              <CardDescription>
+                <span className="font-black text-xl leading-3">•</span> Offline
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <img
+                src="https://via.placeholder.com/200"
+                width={200}
+                height={200}
+                alt="Project Image"
+              />
+            </CardContent>
+          </Card>
+          <Card>
+            <CardHeader>
+              <CardTitle>Project Name</CardTitle>
+              <CardDescription>
+                <span className="font-black text-xl leading-3 text-green-600">
+                  •
+                </span>{" "}
+                Online
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <img
+                src="https://via.placeholder.com/200"
+                width={200}
+                height={200}
+                alt="Project Image"
+              />
+            </CardContent>
+          </Card>
+        </div>
+      </div>
     </>
   );
 }
