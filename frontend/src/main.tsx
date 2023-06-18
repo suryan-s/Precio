eb2431f4fbf0b4ffc3f6db1f7eae9801d78d8d89
Arjun S <arjun8107@gmail.com>
2023-06-19 02:27:38 +0530

    Intro effect

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { TourProvider } from "@reactour/tour";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const steps = [
+  {
+    selector: "#add-new",
+    content: "Click here to add a new project!",
+  },
+];
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <TourProvider
+      showPrevNextButtons={false}
+      showDots={false}
+      showBadge={false}
+      steps={steps}
+    >
+      <App />
+    </TourProvider>
+  </React.StrictMode>
+);
